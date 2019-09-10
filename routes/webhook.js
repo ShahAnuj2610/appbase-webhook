cc71f33042c3c04fa133ea0b196440c7fbe5dd46
anuj <anujshah584@gmail.com>
2019-09-10 18:38:26 +0530

    add dummy response

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
-    console.log(req.query)
+    console.log(req.query);
+    res.send('awesome this works')
 });
 
 module.exports = router;
